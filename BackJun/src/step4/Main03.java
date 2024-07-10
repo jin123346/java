7760ec710a25fb8e46fd52d7036d28621fed3be4
hajhi7899 <hajhi7899@gmail.com>
2024-07-10 21:14:06 +0900

    baekjoon in home

--- BackJun/src/step4/Main03.java
+++ BackJun/src/step4/Main03.java
@@ -1,9 +1,39 @@
 package step4;
 
-public class Main03 {
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
+public class Main03 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		int[]  arr = new int[N];
+		int Max=0;
+		int Min = Integer.MAX_VALUE;
+		st = new StringTokenizer(br.readLine());
+
+		for(int i=0;i<N;i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		for(int i=0;i<arr.length;i++) {
+				if(arr[i]>=Max) {
+					Max = arr[i];
+				}
+				if(arr[i]<=Min) {
+					Min=arr[i];
+				}
+			
+		}
+		
+		System.out.println(Min+" "+Max);
+		
+		
+		
 	}
 
 }
--- BackJun/src/step4/Main05.java
+++
@@ -1,9 +0,0 @@
-package step4;
-
-public class Main05 {
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
-}
