c105c71830d7b59efc6594d82ec61291d5704e4f
hajhi7899 <hajhi7899@gmail.com>
2024-07-11 18:57:41 +0900

    2024/07/11 thisisjava chapter08

---
+++ Thisjava/src/ch08/sec05/RemoteConEx.java
@@ -0,0 +1,14 @@
+package ch08.sec05;
+
+public class RemoteConEx {
+	public static void main(String[] args) {
+		RemoteControl rc;
+
+		rc=new Audio();
+		rc.turnOn();
+		rc.setVolume(5);
+		
+		rc.setMute(true);
+		rc.setMute(false);
+	}
+}
---
+++ Thisjava/src/ch08/sec09/ExtendsEx.java
@@ -0,0 +1,18 @@
+package ch08.sec09;
+
+public class ExtendsEx {
+	public static void main(String[] args) {
+		InterfaceImpl impl = new InterfaceImpl();
+		
+		InterfaceA ia = impl;
+		ia.methodA();
+		System.out.println();
+		
+		InterfaceB ib=impl;
+		ib.methodB();
+		System.out.println();
+		
+		
+	}
+
+}
