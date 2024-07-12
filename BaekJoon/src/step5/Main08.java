11b4280d978e27c93c164ab41e59569e2ade1ce0
hajhi7899 <hajhi7899@gmail.com>
2024-07-12 18:37:28 +0900

    baekjoon study

--- BaekJoon/src/step5/Main08.java
+++ BaekJoon/src/step5/Main08.java
@@ -1,11 +1,27 @@
 package step5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
 /*
  * #날짜 : 2024/07/10
- * #백준 : 27866번 문자와 문자열
+ * #백준 : 1152번 단어의 개수
  */
 public class Main08 {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+		String str = br.readLine().trim();
 		
+		
+		if(str.toString().isEmpty()) {
+			System.out.println(0);
+		}else {
+			String[] words = str.split(" ");
+			System.out.println(words.length);
+
+		}
 	}
 
 }
