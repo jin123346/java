f9e4ec823cd52fcbbf12321b256c77b2c7846395
hajhi7899 <hajhi7899@gmail.com>
2024-07-14 21:03:10 +0900

    home study 2

--- BaekJoon/src/step6/Main03.java
+++ BaekJoon/src/step6/Main03.java
@@ -1,11 +1,34 @@
 package step6;
 
+import java.util.Scanner;
+
 /*
  * #날짜 : 2024/07/
- * #백준 : 번 
+ * #백준 : 2444번 별찍기
  */
 public class Main03 {
 	public static void main(String[] args) {
+		Scanner sc =  new Scanner(System.in);
+		int  N = sc.nextInt();
+		int count=0;
+		
+		for(int i=0;i<2*N-1;i++) {
+			if(i<N) {
+				count++;
+			}else {
+				count--;
+			}
+			for(int j=N-1;j>count-1;j--) {
+				System.out.print(" ");
+			}
+			for(int k=0;k<2*count-1;k++) {
+				System.out.print("*");
+			}
+			System.out.print("\n");
+		}
+		
+		
+		sc.close();
 		
 	}
 	
