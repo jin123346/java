119be34b6b54b463311e1bd8ffb7f63a987ecdde
hajhi7899 <hajhi7899@gmail.com>
2024-08-02 11:45:06 +0900

    modified

--- ch14/src/user3/User3DAO.java
+++ ch14/src/user3/User3DAO.java
@@ -3,6 +3,7 @@ package user3;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
@@ -25,36 +26,54 @@ public class User3DAO {
 	PreparedStatement pstmt =null;
 	
 	
-	public int insertUser(String uid) {
-		String sql = null;
+	public int insertUser(User3VO vo) {
+		String sql = "INSERT INTO `USER3` SET UID = ?, NAME = ?, BIRTH =?, HP=?, ADDR =? ";
 		int result=0;
 		try {
 			conn = DriverManager.getConnection(HOST, USER, PASS);
-			pstmt.executeUpdate(sql);
+			pstmt= conn.prepareStatement(sql);
+			pstmt.setString(1, vo.getUid());
+			pstmt.setString(2, vo.getName());
+			pstmt.setString(3, vo.getBirth());
+			pstmt.setString(4, vo.getHp());
+			pstmt.setString(5, vo.getAddr());
+			
+			result=pstmt.executeUpdate();
+			
+			pstmt.close();
+			
+			try {
+				
+				conn.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 			
 		} catch (Exception e) {
 			// TODO: handle exception
 		}
 		
-		try {
-			pstmt.close();
-			conn.close();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
 		
 		
 		return result;
+		
 		
 	}
 	public int UpdateUser(User3VO vo) {
-		String sql = null;
+		String sql = "update `user3` set  NAME = ?, BIRTH =?, HP=?, ADDR =? where  uid=?";
 		int result=0;
 		try {
 			conn = DriverManager.getConnection(HOST, USER, PASS);
-			pstmt.executeUpdate(sql);
+			pstmt = conn.prepareStatement(sql);
+			
+			pstmt.setString(1, vo.getName());
+			pstmt.setString(2,vo.getBirth());
+			pstmt.setString(3, vo.getHp());
+			pstmt.setString(4, vo.getAddr());
+			pstmt.setString(5, vo.getUid());
+			
+			result = pstmt.executeUpdate();
 			
 		} catch (Exception e) {
 			// TODO: handle exception
@@ -72,12 +91,26 @@ public class User3DAO {
 		
 		return result;
 	}
-	public int selectUser() {
-		String sql = null;
-		int result=0;
+	public User3VO selectUser(String uid) {
+		String sql = "select * from `user3` where uid=?";
+		User3VO user3= null;
 		try {
 			conn = DriverManager.getConnection(HOST, USER, PASS);
-			pstmt.executeUpdate(sql);
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, uid);
+			
+			ResultSet rs = pstmt.executeQuery();
+			
+			if(rs.next()) {
+				user3 =new User3VO();
+				
+				user3.setUid(rs.getString(1));
+				user3.setName(rs.getString(2));
+				user3.setBirth(rs.getString(3));
+				user3.setHp(rs.getString(4));
+				user3.setAddr(rs.getString(5));
+				
+			}
 			
 		} catch (Exception e) {
 			// TODO: handle exception
@@ -93,17 +126,28 @@ public class User3DAO {
 		
 		
 		
-		return result;
+		return user3;
 	}
-	public List<User3VO> selectUsers(String uid) {
-		String sql = null;
-	
-		User3VO user = new User3VO();
+	public List<User3VO> selectUsers() {
+		String sql = "select * from `user3`";
+		User3VO user = null;
 		
 		List<User3VO> users = new ArrayList<>();
 		try {
 			conn = DriverManager.getConnection(HOST, USER, PASS);
-			pstmt.executeUpdate(sql);
+			pstmt = conn.prepareStatement(sql);
+			
+			ResultSet rs = pstmt.executeQuery();
+			
+			while(rs.next()) {
+				user = new User3VO();
+				user.setUid(rs.getString(1));
+				user.setName(rs.getString(2));
+				user.setBirth(rs.getString(3));
+				user.setHp(rs.getString(4));
+				user.setAddr(rs.getString(5));
+				users.add(user);
+			}
 			
 		} catch (Exception e) {
 			// TODO: handle exception
@@ -121,11 +165,17 @@ public class User3DAO {
 		return users;
 	}
 	public int deletetUser(String uid) {
-		String sql = null;
+		String sql = "delete from `user3` where uid = ?";
 		int result=0;
 		try {
 			conn = DriverManager.getConnection(HOST, USER, PASS);
-			pstmt.executeUpdate(sql);
+			pstmt = conn.prepareStatement(sql);
+			
+			pstmt.setString(1, uid);
+			
+			result = pstmt.executeUpdate();
+			
+			
 			
 		} catch (Exception e) {
 			// TODO: handle exception
--- ch14/src/user3/User3Main.java
+++ ch14/src/user3/User3Main.java
@@ -1,5 +1,7 @@
 package user3;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class User3Main {
@@ -9,6 +11,7 @@ public class User3Main {
 		System.out.println("-------------------------------------");
 		
 		Scanner sc = new Scanner(System.in);
+		User3DAO dao = User3DAO.getInstance();
 		
 		while(true) {
 			System.out.println("종료:0, 입력:1, 조회:2, 검색:3, 수정:4, 삭제:5");
@@ -17,8 +20,102 @@ public class User3Main {
 			
 			if(answer==0) {
 				
+				break;
+			}else if(answer==1) {
+				// 입력 insert 
+				User3VO user = new User3VO();
+				System.out.print(" 아이디 입력 >> ");
+				user.setUid(sc.next());
+				System.out.print(" 이름 입력 >> ");
+				user.setName(sc.next());
+				
+				System.out.print(" 생년월일 입력 >> ");
+				user.setBirth(sc.next());
+				
+				System.out.print(" 핸드폰 번호 입력 >> ");
+				user.setHp(sc.next());
+				
+				System.out.print(" 주소 입력 >> ");
+				user.setAddr(sc.next());
+				
+				int result =dao.insertUser(user);
+				
+				if(result > 0) {
+					System.out.println("입력 성공!");
+				}else {
+					System.out.println("입력에 실패하였습니다.");
+				}
+				
+				
+			}else if(answer==2) {
+				//조회 select 
+				List<User3VO> users = new ArrayList<User3VO>();
+				
+				users = dao.selectUsers();
+				
+				for(User3VO vo : users) {
+					System.out.println(vo);
+				}
+				
+				
+			}else if(answer==3) {
+				//검색 select (field)
+				User3VO user = new User3VO();
+				System.out.print(" 검색할 아이디 입력 >> ");
+				String uid= sc.next();
+				
+				user = dao.selectUser(uid);
+				
+				if(user !=null) {
+				System.out.println(user);
+				}else {
+					System.out.println("해당하는 아이디가 없습니다.");
+				}
+				
+				
+			}else if(answer==4) {
+				//수정 update 
+				User3VO user = new User3VO();
+				System.out.print("수정할 아이디 입력 >> ");
+				user.setUid(sc.next());
+				System.out.print("이름 입력 >> ");
+				user.setName(sc.next());
+				
+				System.out.print("생년월일 입력 >> ");
+				user.setBirth(sc.next());
+				
+				System.out.print("핸드폰 번호 입력 >> ");
+				user.setHp(sc.next());
+				
+				System.out.print("주소 입력 >> ");
+				user.setAddr(sc.next());
+				
+				int result = dao.UpdateUser(user);
+				if(result > 0) {
+					System.out.println("업데이트 성공!");
+				}else {
+					System.out.println("업데이트 실패!");
+				}
+				
+				
+			}else if(answer==5) {
+				//삭제 delete
+				System.out.print("삭제할 아이디 입력 >>");
+				String uid= sc.next();
+				
+				
+				int result= dao.deletetUser(uid);
+				
+				if(result>0) {
+					System.out.println(uid + " 삭제 성공!");
+				}else {
+					System.out.println("삭제 실패ㅜ.ㅜ");
+				}
 			}
 		}
+		System.out.println("프로그램 종료");
+		
+		
 	}
 
 }
